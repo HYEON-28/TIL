18bcb3540857fc429f9346197268c426d3dc08d2
HYEON-28 <yeseol.lee28@gmail.com>
2026-04-10 19:22:11 +0900

    리액트 2차시

--- frontend/doit_project/ch02/ch02_1/src/index.tsx
+++ frontend/doit_project/ch02/ch02_1/src/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(
-  // react 18버전
-  document.getElementById('root') as HTMLElement
-)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+let pPhysicalDOM = document.createElement('p')
+pPhysicalDOM.innerText = 'Hello physical DOM world!'
+// document.body.appendChild(pPhysicalDOM)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+const pVirtualDOM = React.createElement('p', null, 'Hello world!')
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(pVirtualDOM)
