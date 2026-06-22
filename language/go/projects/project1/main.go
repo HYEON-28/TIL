4caadf7e60e4bbf607cf0ee201dabf50785b6db7
HYEON-28 <yeseol.lee28@gmail.com>
2026-06-22 10:01:11 +0900

    go: Dictionary - Add Method

--- language/go/projects/project1/main.go
+++ language/go/projects/project1/main.go
@@ -7,12 +7,18 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First word"}
+	word := "hello"
+	definition := "Greeting"
 
-	definition, err := dictionary.Search("first")
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+	}
+	hello, _ := dictionary.Search(word)
+	fmt.Println(hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
 	}
 }
 
--- language/go/projects/project1/mydict/mydict.go
+++ language/go/projects/project1/mydict/mydict.go
@@ -1,12 +1,15 @@
 package mydict
 
-import "errors"
+import (
+	"errors"
+)
 
 // Dictionary type -> 기존 타입에 의미와 메서드를 부여할 때 사용함
 // struct는 여러 데이터를 묶어서 하나의 객체처럼 다룰 때
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word alreay exists")
 // type Dictionary struct{
 // }
 // 위는 map을 기반한 새로운 타입, 이것은 struct 타입.
@@ -18,4 +21,17 @@ func (d Dictionary) Search(word string) (string, error){
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+
+func (d Dictionary) Add(word string, def string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists	
+	}
+	return nil;
 }
